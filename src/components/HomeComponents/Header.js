6f40858f1ddc23850a0b386fc5192644247feb9d
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-12-05 16:20:34 +0100

    ThreeColumns done

--- src/components/HomeComponents/Header.js
+++ src/components/HomeComponents/Header.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {NavLink} from "react-router-dom";
 // import Link from "react-scroll";
 
-import * as Scroll from 'react-scroll';
-import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
+import {Link} from 'react-scroll';
 
 
 class Header extends Component {
--- src/components/HomeComponents/ThreeColumns.js
+++ src/components/HomeComponents/ThreeColumns.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react';
+import {Element} from 'react-scroll'
 
 class ThreeColumns extends Component {
     render() {
-        return <h1>3columns</h1>
+        return (
+            <section className='threeColumns'>
+                <div className='container'>
+                    <div className='box'>
+                        <h1>10</h1>
+                        <h3>Oddanych worków</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div><div className='box'>
+                        <h1>5</h1>
+                        <h3>wspartych organizacji</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div><div className='box'>
+                        <h1>7</h1>
+                        <h3>zorganizowanych zbiórek</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+
+                </div>
+            </section>
+        )
     }
 }
 
