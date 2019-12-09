ec3433a02bc06e74c48eb5271dae64a8238eeac0
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-12-09 13:20:03 +0100

    locals

--- src/lists/locals.js
+++ src/lists/locals.js
@@ -1,13 +1,13 @@
-function Lokal(name, description, array) {
+function Local(name, description, array) {
     this.name = name;
     this.description = description;
     this.items = array;
 }
 
 const locals = [
-    new Lokal('Zbiórka lokalna jeden', 'To jest zbiórka lokalna numer jeden', ['lorem', 'ipsum', 'dolor']),
-    new Lokal('Zbiórka lokalna dwa', 'To jest zbiórka lokalna numer dwa', ['lorem', 'dolor']),
-    new Lokal('Zbiórka lokalna trzy', 'To jest zbiórka lokalna numer trzy', ['dolor']),
+    new Local('Zbiórka lokalna jeden', 'To jest zbiórka lokalna numer jeden', ['lorem', 'ipsum', 'dolor']),
+    new Local('Zbiórka lokalna dwa', 'To jest zbiórka lokalna numer dwa', ['lorem', 'dolor']),
+    new Local('Zbiórka lokalna trzy', 'To jest zbiórka lokalna numer trzy', ['dolor']),
 ];
 
 export default locals
