7d69858e5dc870b3fcc93ec35b1e144bb753d3a3
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-12-12 15:52:19 +0100

    Login window done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 import Navigation from "./Navigation";
+import {NavLink} from "react-router-dom";
 
 class Login extends Component {
     render() {
         return (
             <>
                 <Navigation />
-                <h1>Login</h1>
+                <div className='container'>
+                    <div className='login'>
+                        <h1>Zaloguj się</h1>
+                        <img src="/assets/Decoration.svg" alt="decoration"/>
+                        <form>
+                            <p>E-mail</p>
+                            <input type="text"/>
+                            <p>Hasło</p>
+                            <input type="password"/>
+                        </form>
+                    </div>
+                    <div className='buttons'>
+                        <span><NavLink to='register'>Załóż konto</NavLink></span>
+                        <span>Zaloguj się</span>
+                    </div>
+                </div>
             </>
         )
     }
